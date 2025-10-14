d23e0532d38714b0efc0c337781b16f6f8a9cc5c
Subsystem2418 <e.soto13@ufromail.cl>
2025-10-14 18:03:28 -0300

    Se inicializa trabajo en LogIng

---
+++ ui-ctrlAsistencia/ctrl-asistencia/src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Ctrl Asistencia. Todos los derechos reservados.</p>
+      <p>&copy; Estamos trabajando para usted...</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ ui-ctrlAsistencia/ctrl-asistencia/src/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Bienvenido al Control de Asistencia 2025 4k no fake</h1>
+    </div>
+  );
+};
+
+export default Home;
